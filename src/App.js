7861e0b07cf5300ffa79d4c96d80ec6b17fa9c33
花裤熊二 <404437516@qq.com>
2019-04-17 17:39:27 +0800

    提价

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.less';
+import {Button} from 'antd'
+class App extends Component {
+  render() {
+    return (
+      <div className='content'>
+          hello world
+        <Button>获取</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
